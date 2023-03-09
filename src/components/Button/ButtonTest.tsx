989659a66dbb4e4cd426fac68503d3aec06a3f47
RaulMullor <mullor92@gmail.com>
2023-03-09 13:55:10 +0100

    create Button test

---
+++ src/components/Button/ButtonTest.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+
+describe("Given a Button component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a button with the text 'Log in'", () => {
+      const buttonText = "Log in";
+
+      render(<Button text={buttonText.toUpperCase()} />);
+
+      const expectedButton = screen.getByRole("button", {
+        name: buttonText.toUpperCase(),
+      });
+
+      expect(expectedButton).toBeInTheDocument();
+    });
+  });
+});
