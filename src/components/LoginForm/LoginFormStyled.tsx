1a45e84e22ad90a1ae1216ef971f0aac73f1370f
RaulMullor <mullor92@gmail.com>
2023-03-09 12:01:18 +0100

    add styled form login

---
+++ src/components/LoginForm/LoginFormStyled.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const LoginFormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 44px;
+  align-items: center;
+  .login-form {
+    &__section {
+      display: flex;
+      flex-direction: column;
+      gap: 6px;
+    }
+    &__field {
+      height: 3.5rem;
+      width: 18.75rem;
+      border: 1px solid #000;
+      border-radius: 12px;
+      padding: 0 14px;
+    }
+  }
+`;
+
+export default LoginFormStyled;
