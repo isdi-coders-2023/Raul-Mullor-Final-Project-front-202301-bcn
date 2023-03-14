8a4b3a6f6a271df7dbe9ad6fdca7e71a84925550
RaulMullor <mullor92@gmail.com>
2023-03-14 12:28:37 +0100

    create moked card

---
+++ src/mocks/Card.tsx
@@ -0,0 +1,19 @@
+import {
+  Alignment,
+  CharacterDataStructure,
+  Classes,
+  Races,
+} from "../types/characters/types";
+
+export const mockCharacter: CharacterDataStructure = {
+  name: "Victor",
+  age: 32,
+  gender: "male",
+  height: "1.76",
+  weight: "65kg",
+  alignment: Alignment.ChaoticEvil,
+  race: Races.human,
+  pathfinderclass: Classes.monk,
+  image: "https://avatars.githubusercontent.com/u/109736378?v=4",
+  id: "s4l4m1v3g4n0",
+};
