dfb9e60d73079675eb7f4daf1fe366bffccffe8b
RaulMullor <mullor92@gmail.com>
2023-03-14 12:29:05 +0100

    create card test

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,16 @@
+import { screen } from "@testing-library/react";
+import { mockCharacter } from "../../mocks/Card";
+import { renderWithProviders } from "../../testUtils";
+import Card from "./Card";
+
+describe("Given a card component", () => {
+  describe("When its rendered", () => {
+    test("Then it should show a image", () => {
+      renderWithProviders(<Card character={mockCharacter} />);
+
+      const expectedImage = screen.getByRole("img");
+
+      expect(expectedImage).toBeInTheDocument();
+    });
+  });
+});
