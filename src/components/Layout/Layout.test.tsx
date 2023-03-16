d100c9bfce8bd3062a6061bdbd20758e07ae6006
RaulMullor <mullor92@gmail.com>
2023-03-16 13:42:51 +0100

    refactor Layout test

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -2,12 +2,14 @@ import { screen } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import theme from "../../styles/Theme";
 import { renderRouterWithProviders } from "../../testUtils";
+import { UserState } from "../../types/user/types";
 import Header from "../Header/Header";
 
 describe("Given a Layout component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a banner", () => {
       renderRouterWithProviders(
+        {},
         <ThemeProvider theme={theme}>
           <Header />
         </ThemeProvider>
